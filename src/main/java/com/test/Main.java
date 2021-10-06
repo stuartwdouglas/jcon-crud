8a1424c3a37e3d3cf3f6aa362c7b7d967ecde0c9
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-10-07 10:27:40 +1100

    more

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.awt.geom.QuadCurve2D;
+
+import io.quarkus.runtime.Quarkus;
+
+public class Main {
+
+    public static void main(String ... args) {
+        Quarkus.run(args);
+    }
+
+}
